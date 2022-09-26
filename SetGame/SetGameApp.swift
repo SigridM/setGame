def8a12f714b22d7fd40d219f4ce86439eabb415
SigridM <sigridm@selfhelpsoftware.com>
2022-09-26 10:57:07 -0400

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Sigrid Mortensen on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
