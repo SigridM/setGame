c5998d0cc2d132b0f26fecd83dacf4df8f010b97
SigridM <sigridm@selfhelpsoftware.com>
2022-10-06 13:18:00 -0400

    Commented SetCard and SetGameConstants

--- SetGame/Models/SetCard.swift
+++ SetGame/Models/SetCard.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+/// A struct for encapsulating the structure and behavior of a single card in the game of Set. Each SetCard has four features (shape,
+/// shading, color, and number of symbols) and can determine whether it forms a set with any other two SetCards. It also has a mutable
+/// state: a Boolean indicating whether the SetCard is currently selected.
 struct SetCard: Equatable {
     let shape: ShapeFeature
     let shading: ShadeFeature
@@ -15,17 +18,29 @@ struct SetCard: Equatable {
     
     var isSelected = false
     
+    /// Answers a Boolean: whether the receiver forms a set with two other cards.
+    /// - Parameters:
+    ///   - secondCard: a SetCard (that must not be the receiver) that is potentially part of the set
+    ///   - thirdCard: a SetCard (that must not be the receiver or the secondCard) that is potentially part of the set
+    /// - Returns: a Boolean, true if the three cards together form a proper set
     func formsSetWith(_ secondCard: SetCard, and thirdCard: SetCard) -> Bool {
-        self.shape.formsSetWith(secondCard.shape, and: thirdCard.shape) &&
-        self.shading.formsSetWith(secondCard.shading, and: thirdCard.shading) &&
-        self.color.formsSetWith(secondCard.color, and: thirdCard.color) &&
-        self.number.formsSetWith(secondCard.number, and: thirdCard.number)
+        
+        guard secondCard != self && thirdCard != self && secondCard != thirdCard else {
+            return false
+        }
+        return
+            self.shape.formsSetWith(secondCard.shape, and: thirdCard.shape) &&
+            self.shading.formsSetWith(secondCard.shading, and: thirdCard.shading) &&
+            self.color.formsSetWith(secondCard.color, and: thirdCard.color) &&
+            self.number.formsSetWith(secondCard.number, and: thirdCard.number)
     }
     
+    /// Turns selection on if it is off, and vice versa
     mutating func toggleSelection() {
         isSelected = !isSelected
     }
     
+    /// A unique Int that identifies a single SetCard
     var id: Int {
         return shape.rawValue * 1000
         + shading.rawValue * 100
--- SetGame/Models/SetGameConstants.swift
+++ SetGame/Models/SetGameConstants.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
+/// A struct for encapuslating the constants in a Set game.
 struct SetGameConstants {
+    /// The number of cards that make up a set
     static let setSize = 3
+    /// The number of cards that should be dealt when the game begins.
     static let initialDealSize = setSize * 4
 }
