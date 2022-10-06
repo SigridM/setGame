43a507365980933a1b9b914c4f70e6c43112f0c0
SigridM <sigridm@selfhelpsoftware.com>
2022-10-06 13:25:08 -0400

    Commented the SetFeature protocol.

--- SetGame/Models/SetFeature.swift
+++ SetGame/Models/SetFeature.swift
@@ -7,9 +7,27 @@
 
 import Foundation
 
-protocol SetFeature: Equatable, CaseIterable {}
+/// A protocol that defines the behavior of an individual feature (shape, shade, color, or number) of a Set game card.
+protocol SetFeature: Equatable, CaseIterable {
+    /// A SetFeature must be able to determine whether it forms a set with two other features of the same type
+    /// - Parameters:
+    ///   - secondFeature: a SetFeature that is of the same type as the receiver and is a candidate for forming a set with the
+    ///   third feature and the receiver.
+    ///   - thirdFeature: a SetFeature that is of the same type as the receiver and is a candidate for forming a set with the
+    ///   second feature and the receiver.
+    /// - Returns: a Boolean, true if all three features form a proper set
+    func formsSetWith(_ secondFeature: Self, and thirdFeature: Self) -> Bool
+}
 
 extension SetFeature {
+    /// The default behavior that determines whether it forms a set with two other features of the same type
+    /// - Parameters:
+    ///   - secondFeature: a SetFeature that is of the same type as the receiver and is a candidate for forming a set with the
+    ///   third feature and the receiver.
+    ///   - thirdFeature: a SetFeature that is of the same type as the receiver and is a candidate for forming a set with the
+    ///   second feature and the receiver.
+    /// - Returns: a Boolean, true if all three features form a proper set. It is a proper set if either all features are the same, or all
+    /// features are different.
     func formsSetWith(_ secondFeature: Self, and thirdFeature: Self) -> Bool {
         if self == secondFeature && self == thirdFeature {
             return true
@@ -20,24 +38,28 @@ extension SetFeature {
         return false
     }
 }
+/// A SetFeature that is one of three different shapes. Has a rawValue so the feature can be used to form a unique ID for a SetCard.
 enum ShapeFeature: Int, SetFeature {
     case shape1 = 1
     case shape2 = 2
     case shape3 = 3
 }
 
+/// A SetFeature that is one of three different shades. Has a rawValue so the feature can be used to form a unique ID for a SetCard.
 enum ShadeFeature: Int, SetFeature {
     case shade1 = 1
     case shade2 = 2
     case shade3 = 3
 }
 
+/// A SetFeature that is one of three different colors. Has a rawValue so the feature can be used to form a unique ID for a SetCard.
 enum ColorFeature: Int, SetFeature {
     case color1 = 1
     case color2 = 2
     case color3 = 3
 }
 
+/// A SetFeature that is one of three different numbers. Has a rawValue so the feature can be used to form a unique ID for a SetCard.
 enum NumberFeature: Int, SetFeature {
     case one = 1
     case two = 2
